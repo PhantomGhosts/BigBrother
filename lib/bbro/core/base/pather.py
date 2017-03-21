85e12ad472bbd9e2ff53c293ff3c2ad9fae30a84
federico123579 <federico123579@gmail.com>
2017-03-21 20:46:11 +0100

    Added docstring and renamed function

--- lib/bbro/core/base/pather.py
+++ lib/bbro/core/base/pather.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
+'''
+This module contains main complex functions that operate over relative
+pathes.
+'''
+
 import os.path
 
 class PATHanlder(object):
 	def __init__(self, file):
 		self.file = file
 
-	def relative_dir(self):
+	def parent_dir(self):
+		'''
+		return parent directory
+		'''
 		return os.path.abspath(os.path.dirname(self.file))
