f91c5ec9d6d8843c2855c67807ae5796408fd4af
federico123579 <federico123579@gmail.com>
2017-03-29 23:56:00 +0200

    updated config

--- config/config.py
+++ config/config.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 
-import os.path
-#!/usr/bin/env python
-
 '''
 Config file
 '''
 
 import yaml
+import os.path
+from lib.base import pather
 
-info_file = open(os.path.join(os.path.abspath('.'), 'info.yml'), 'r')
+PATHandler = pather.PATHandler(__file__)
+pardir = PATHandler.parent_dir()
+
+info_file = open(os.path.join(pardir, 'info.yml'), 'r')
 config_info = yaml.load(info_file)
 info_file.close()
-database_file = open(os.path.join(os.path.abspath('.'), 'database.yml'), 'r')
+database_file = open(os.path.join(pardir, 'database.yml'), 'r')
 config_database = yaml.load(database_file)
 database_file.close()
-urls_file = open(os.path.join(os.path.abspath('.'), 'urls.yml'), 'r')
+urls_file = open(os.path.join(pardir, 'urls.yml'), 'r')
 config_urls = yaml.load(urls_file)
 urls_file.close()
 
@@ -32,4 +34,4 @@ class database(object):
 
 class url(object):
     main_server = config_urls['main_server']
-    repo_url = config_urls['repo_url']
+    repo = config_urls['repo_url']
